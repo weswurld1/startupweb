50609df721ccd4969a49b98df3b81489a1e187ea
weswurld1 <wesleyariascarranza@gmail.com>
2026-02-28 15:35:46 -0600

    Add Google Fonts links to layout component
    Added Google Fonts preconnect links and stylesheet.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
